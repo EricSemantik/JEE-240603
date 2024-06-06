0af5649c8fe9b4296a495883481bfab774fc5ffb
AzureAD\EricSULTAN <eric@fifox.fr>
2024-06-06 10:46:45 +0200

    eshop-web-v3

--- eshop-web/src/main/java/dev/formation/web/AdresseController.java
+++ eshop-web/src/main/java/dev/formation/web/AdresseController.java
@@ -2,6 +2,7 @@ package dev.formation.web;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import dev.formation.Application;
 import dev.formation.model.Adresse;
@@ -43,28 +44,36 @@ public class AdresseController extends HttpServlet {
 	private void list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// ETAPE 2 : Récupérer les données
 		List<Adresse> adresses = Application.getInstance().getAdresseRepository().findAll();
-		
+
 		// ETAPE 3 : Renseigner le Model
 		request.setAttribute("mesAdresses", adresses);
-		
+
 		// ETAPE 4 : Appel de la View
 		getServletContext().getRequestDispatcher("/WEB-INF/views/adresse/list.jsp").forward(request, response);
 	}
 
-	private void add(HttpServletRequest request, HttpServletResponse response) {
-
+	private void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		getServletContext().getRequestDispatcher("/WEB-INF/views/adresse/form.jsp").forward(request, response);
 	}
 
-	private void edit(HttpServletRequest request, HttpServletResponse response) {
-
+	private void edit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Long id = request.getParameter("id")!=null?Long.valueOf(request.getParameter("id")):null;
+		
+		Optional<Adresse> optAdresse = Application.getInstance().getAdresseRepository().findById(id);
+		
+		if(optAdresse.isPresent()) {
+			request.setAttribute("adresse", optAdresse.get());
+		}
+		
+		getServletContext().getRequestDispatcher("/WEB-INF/views/adresse/form.jsp").forward(request, response);
 	}
 
 	private void save(HttpServletRequest request, HttpServletResponse response) {
 
 	}
 
-	private void cancel(HttpServletRequest request, HttpServletResponse response) {
-
+	private void cancel(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		getServletContext().getRequestDispatcher("/adresse?mode=list").forward(request, response);
 	}
 
 	private void remove(HttpServletRequest request, HttpServletResponse response) {
